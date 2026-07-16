ce78dce0cbd72a6a6ee182152c2f469b5b36208b
SaminFlicy <ibrahimjayed2312@gmail.com>
2026-07-16 22:41:11 +0600

    CW or HW

---
+++ Operators.java
@@ -0,0 +1,18 @@
+public class Operators {
+
+    public static void main(String[] args) {
+
+      int a = 6;
+      int b = 7;
+      int sum = a + b;  
+      System.out.println("The sum of a and b is: " + sum);
+
+     int sub = b - a;
+     System.out.println("The subtraction of a and b is: " + sub);
+
+     
+
+    }
+
+}
+                                                                                                                                                                                                                            
